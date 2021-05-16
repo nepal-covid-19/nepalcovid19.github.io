e6c32095bb8607963630cc62540ab606a358f983
developer <sanjaymundhra1988@gmail.com>
2021-05-16 18:55:19 +0530

    render table data as per the selected box and district

---
+++ js/main.js
@@ -0,0 +1,63 @@
+$(document).ready(function () {
+    var place = $('select').val();
+    // var url = 'data/table-beds.json';
+    // $table = $('#table-beds');
+    // $table_ambulance = $('#table-ambulance');
+    // $table_remdesivir = $('#table-remdesivir');
+    // $table_testing = $('#table-testing');
+    tables = ['beds','ambulance','remdesivir','testing'];
+    
+    $.each(tables, function(i,tab){
+        url = 'data/table-'+tab+'.json';
+        $table = $('#table-'+tab);
+        init_table(url, $table);
+    })
+
+    $(".parent").on("click", function () {
+      $('.parent').removeClass('highlight');
+      $(this).addClass('highlight');
+      $id = this.id;
+      $table = $('#table-'+$id);
+      $('.bootstrap-table').hide();
+      $table.closest('.bootstrap-table').show();
+      refresh_table($table);
+    });
+
+    function update_url(id) {
+      $table.bootstrapTable('load',{
+        url: 'data/table-'+id+'.json'
+      })
+    }
+    
+    function init_table(url, $table){
+      $table.bootstrapTable({
+        showColumns: true,
+        search: true,
+        showRefresh: true,
+        url: url,
+        responseHandler: function (result) {
+          json = [];
+          if(place && (place in result)){
+            json = result[place];
+          }
+          return json;
+        }
+      });
+    }
+
+    $('select').on('change',function(){
+      place = this.value;  
+      $id = $(".highlight")[0].id;log($id)
+      $table = $('#table-'+$id);    
+      refresh_table($table);
+    });
+
+  });
+
+  function refresh_table($table){
+    $table.bootstrapTable('refresh');
+  }
+
+  function log($log){
+    console.log($log);
+  }
